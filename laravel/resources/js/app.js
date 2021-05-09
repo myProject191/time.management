ced922f9df86252eab2b0a4cba49b7dc54ef2ed3
DaikiSawada-web <sawada@i.social-db.co.jp>
2021-05-09 11:19:37 +0900

    vueの雛形作成

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,3 +1,11 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+
+import Vue from 'vue'
+import Sample from './components/Sample.vue'
+new Vue({
+    el: '#app',
+    components: { Sample }
+});
